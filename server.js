0da4a488c9c01bbeb00b134174816e794cab65e0
Anjali <anjaliprema4321@gmail.com>
2025-08-29 23:59:34 +0530

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors());
 // app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
-const MONGO_URI = "mongodb+srv://anjaliprema4321:AnjaliMess@cluster0.ravzlbx.mongodb.net/MessMenu?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URI = "mongodb+srv://<username>:<Password>@cluster0.ravzlbx.mongodb.net/MessMenu?retryWrites=true&w=majority&appName=Cluster0";
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
